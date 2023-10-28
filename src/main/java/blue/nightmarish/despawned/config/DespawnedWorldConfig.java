13057bc32373f915c456c79e02cb0ce6aeaca422
NightmarishBlue <616midnightblue@gmail.com>
2023-10-28 12:32:35 +0100

    Initial commit.

---
+++ src/main/java/blue/nightmarish/despawned/config/DespawnedWorldConfig.java
@@ -0,0 +1,21 @@
+package blue.nightmarish.despawned.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class DespawnedWorldConfig {
+    public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC;
+
+    public static final ForgeConfigSpec.ConfigValue<Integer> CREATURE_SPAWN_CAP;
+
+    static {
+        BUILDER.push("per-world configuration for overspawn");
+        CREATURE_SPAWN_CAP = BUILDER
+                .comment("the maximum spawn cap")
+                .comment("if left at 0, the global default will be used")
+                .defineInRange("World Creature Spawn Cap", 0, 0, 1000); // dunno how to make it greater than 0 without a max.
+        BUILDER.pop();
+        SPEC = BUILDER.build();
+    }
+    
+}
